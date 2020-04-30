a01a45ecbedfab2787af63842ede7b1f47f1b98a
pilartorr <sauleturme@gmail.com>
2020-04-30 11:05:51 +0200

    created the logic for leader-board component

--- src/components/LeaderBoard.js
+++ src/components/LeaderBoard.js
@@ -1,8 +1,11 @@
 import React, { Component, Fragment } from 'react'
 import LogOut from './LogOut'
+import { connect } from 'react-redux'
 
 class LeaderBoard extends Component {
     render() {
+        const { usersList } = this.props
+        console.log(usersList)
         return (
             <Fragment>
                 <LogOut />
@@ -16,12 +19,14 @@ class LeaderBoard extends Component {
                         </tr>
                     </thead>
                     <tbody>
-                        <tr className="text-center">
-                            <td>user.name</td>                       
-                            <td>user.answers</td>
-                            <td>user.questions</td>
-                            <td>total</td>
-                        </tr>
+                        {usersList.map((user, index) => (
+                            <tr className="text-center" key={index}>
+                                <td>{user.user}</td>                       
+                                <td>{user.questionsAnswered}</td>
+                                <td>{user.questionsCreated}</td>
+                                <td>{user.total}</td>
+                            </tr>
+                        ))}                       
                     </tbody>
                 </table>
             </Fragment>
@@ -29,4 +34,18 @@ class LeaderBoard extends Component {
     }
 }
 
-export default LeaderBoard
+function mapStateToProps ( { users } ) {
+    const usersList = Object.keys(users).map((user) => {
+        return {
+          user,
+          questionsCreated: Object.keys(users[user].questions).length,
+          questionsAnswered: Object.keys(users[user].answers).length,
+          total: Object.keys(users[user].questions).length + Object.keys(users[user].answers).length
+        }  
+    }).sort((a, b) => (b.questionsCreated + b.questionsAnswered) - (a.questionsCreated + a.questionsAnswered))
+    return {
+        usersList
+    }
+}
+
+export default connect(mapStateToProps)(LeaderBoard)
