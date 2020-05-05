beab2c2cdd19c12e133c6664047865e0df3228af
pilartorr <sauleturme@gmail.com>
2020-05-05 11:59:53 +0200

    redirect to not-found

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,13 +1,18 @@
 import React, { Component, Fragment } from 'react'
+import { Redirect } from 'react-router-dom'
 
 class NotFound extends Component {
     render() {
-        return (
-            <Fragment>
-                <p className="text-center">Oops, page not found!</p>
-                <p className="text-center">404 Error</p>
-            </Fragment>
-        )
+        if (!this.props.error) {
+            return (
+                <Fragment>
+                    <Redirect to='/not-found' />
+                    <p className="text-center">Oops, page not found!</p>
+                    <p className="text-center">404 Error</p>
+                </Fragment>
+            )
+        }
+
     }
 }
 
